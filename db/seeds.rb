e2a034f6811d86b57e9667b0bee82a92b9b7f088
TianJiang <riersy2324@gmail.com>
2016-11-15 17:49:49 -0600

    add seeds.rb to demonstrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Photo.delete_all
+Photo.create(title: 'A1', img_url: 'md2.jpg',tag_list: 't1, t2')
+Photo.create(title: 'A2', img_url: 'md3.jpg',tag_list: 't2, t3, t6')
+Photo.create(title: 'A3', img_url: 'md4.jpg',tag_list: 't3, t4')
+Photo.create(title: 'A4', img_url: 'md5.jpg',tag_list: 't4, t5, t6')
